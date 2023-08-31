edd3f43308948e94c28595dafaa99e11110289b6
Vibin K <108916451+vibin17604@users.noreply.github.com>
2023-08-31 15:37:16 +0530

    Delete online_meeting_scheduling_app directory

--- online_meeting_scheduling_app/src/main/java/com/example/demo/repository/ParticipantsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Participants;
-
-public interface ParticipantsRepo extends JpaRepository<Participants, Integer> {
-
-}
